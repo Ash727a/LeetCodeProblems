dc3c2f8a0fe7c7d5e3505151b109f14b02ab29f1
ash <148178202+Ash727a@users.noreply.github.com>
2023-12-01 21:27:58 -0800

    Update palidrome.cpp

--- palidrome.cpp
+++ palidrome.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
-        if(x < 0) return false;
-
         unsigned int val =  0; 
         unsigned int xt   = x; 
         
         for(;;)
         {
             if(xt <= 0) break;
-            val = (val* 10) + (xt % 10); 
-            xt  = (xt - (xt % 10)) / 10; 
+            val = (val * 10) + (xt % 10); 
+            xt  = xt - (xt % 10); 
+            xt /= 10; 
         }
-       return x == val; 
+       return !(x ^ val); 
     }
 };
-
